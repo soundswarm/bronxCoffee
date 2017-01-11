fdde72fcb14b44844d106f01fa46c1820abd0a00
Sean Connor <seantconnor@gmail.com>
2017-01-10 18:25:58 -0800

    1st version

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <a href="#">
+          <div className="logo">
+            <div className="logo-text">
+              <div className="first">
+                BRONX
+              </div>
+              <div className="second">
+                COFFEE
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
     );
   }
